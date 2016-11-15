00da3b752a0d1edcc0fd0f0a076ff4c1b2a041e4
LIU xuebao <buptlxb@gmail.com>
2016-11-15 19:14:11 +0800

    remove trash

--- build/finite_automaton.d
+++
@@ -1,2 +0,0 @@
-finite_automaton.o: src/finite_automaton.cpp include/finite_automaton.h \
- include/token.h include/utility.h include/utility.h
--- build/regex.d
+++
@@ -1,2 +0,0 @@
-regex.o: src/regex.cpp include/finite_automaton.h include/token.h \
- include/utility.h
